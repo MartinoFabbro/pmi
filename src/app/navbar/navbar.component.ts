3ac3c8e2c4e0db81fb1b13e159458feb5497b49c
Martino Fabbro <martino.fabbro@gmail.com>
2021-09-10 18:32:39 +0200

    10

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -10,6 +10,15 @@ export class NavbarComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    this.animation()
+  }
+
+  public animation() {
+    $(document).ready(function(){
+      $('#nav-icon1,#nav-icon2,#nav-icon3,#nav-icon4').click(function(){
+        $(this).toggleClass('open');
+      });
+    });
   }
 
 }
